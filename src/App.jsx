b5e9f67cb0e4295da4e7fb39e7b60e6b2927c772
Andrei <andreiferreira@outlook.com>
2022-07-17 13:30:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Header } from './components/Header'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css'
+import './global.css'
+export function App() {
+
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author='Andrei'
+            content='Loren Ipson'
+          ></Post>
+        </main>
+      </div>
+    </div>
+  )
+}
+
---
+++ src/components/Post.jsx
@@ -0,0 +1,24 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src='https://avatars.githubusercontent.com/u/44874956?v=4'></img>
+                    <div className={styles.authorInfo}>
+                        <strong>Andrei Ferreira</strong>
+                        <span>Web Development</span>
+                    </div>
+                </div>
+                <time title='11 de maio as 8h00' dateTime='2022-05-11 08:00:00'>Publicado há 1h</time>
+            </header>
+            <div className={styles.content}>
+                <p>fala galera</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz noNLW return, evento muito doido</p>
+                <p><a href=''>andrei.ferreira/opa</a></p>
+                <p><a href=''>#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article>
+    )
+}
